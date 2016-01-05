a074ab056ec0a62e52145a5c543274f70ea04e23
Enrico Crema <enrico.crema@gmail.com>
2016-01-05 11:24:12 +0000

    Fixed bug for BC/AD dates

--- singleDateCalibration/oxcalSingleDate.R
+++ singleDateCalibration/oxcalSingleDate.R
@@ -39,8 +39,49 @@ oxcalSingleDate<-function(id="tmp01",date,error,oxcalName="singleDate",OxCalDire
         prob=as.numeric(strsplit(prob,split=", ")[[1]])
 
         x=seq(from=start,by=+resolution,length.out=length(prob))
-        if (BP==TRUE) {x=abs(x)+1950}
-        if (plot==TRUE)
-            {plot(x,y=prob*probNorm, xlim=c(max(x),min(x)), type="l",xlab="cal BP",ylab="density",main="")}        
-        return(list(data.frame(x=x,y=prob*probNorm)))
+        if (BP==TRUE)
+            {x=-x+1950
+             if (plot==TRUE)
+                 {
+                     plot(x,y=prob*probNorm, xlim=c(max(x),min(x)),
+                          type="l",xlab="BP",ylab="density",main="",lwd=0)
+                     polygon(x=c(x,rev(x)),y=c(prob*probNorm,rep(0,length=length(x))),border=NA,col="darkgrey")
+                 }
+             }
+
+        if (BP==FALSE)
+            {
+                if (all(x>=0))
+                    {
+                        plot(x,y=prob*probNorm, xlim=c(max(x),min(x)),
+                             type="l",xlab="CE",ylab="density",main="",lwd=0)
+                        polygon(x=c(x,rev(x)),y=c(prob*probNorm,rep(0,length=length(x))),border=NA,col="darkgrey")
+
+                    }
+                if (all(x<=0))
+                    {
+                        plot(x,y=prob*probNorm, xlim=c(min(x),max(x)),
+                             type="l",xlab="BCE",ylab="density",main="",axes=FALSE,lwd=0)
+                        polygon(x=c(x,rev(x)),y=c(prob*probNorm,rep(0,length=length(x))),border=NA,col="darkgrey")
+                        axis(side=2)
+                        axis(side=1,at=pretty(x),labels=abs(pretty(x)))
+                        box()
+                    }
+                if (any(x<=0)&any(x>=0))
+                    {
+                        plot(x,y=prob*probNorm, xlim=c(min(x),max(x)),
+                             type="l",xlab="BCE/CE",ylab="density",main="",axes=FALSE,lwd=0)
+                        polygon(x=c(x,rev(x)),y=c(prob*probNorm,rep(0,length=length(x))),border=NA,col="darkgrey")
+                        axis(side=2)
+                        labels=abs(pretty(x))
+                        labels[which(labels==0)]="1BCE/1CE"
+                        axis(side=1,at=pretty(x),labels=labels)
+                        box()
+                    }
+            }
+
+
+        
+        
+          return(list(data.frame(x=x,y=prob*probNorm)))
     }
